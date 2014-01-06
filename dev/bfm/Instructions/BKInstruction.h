35391720c16c518399e2d5341f86d895390a8839
detomon <simon@monoxid.ch>
2014-01-06 11:26:56 +0100

    Update license header

--- dev/bfm/Instructions/BKInstruction.h
+++ dev/bfm/Instructions/BKInstruction.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012-2013 Simon Schoenenberger
+ * Copyright (c) 2012-2014 Simon Schoenenberger
  * http://blipkit.monoxid.net/
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
