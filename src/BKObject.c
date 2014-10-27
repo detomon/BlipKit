8e7bddd80b201fac7da83739a46395c1a6bde3db
detomon <simon@monoxid.ch>
2014-10-27 15:25:45 +0100

    Add generic object

---
+++ src/BKObject.c
@@ -0,0 +1,162 @@
+/**
+ * Copyright (c) 2012-2014 Simon Schoenenberger
+ * http://blipkit.monoxid.net/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "BKObject.h"
+
+BKInt BKObjectInit (void * object, BKClass const * isa, BKSize guardSize)
+{
+	BKObject * obj = object;
+
+	if (obj == NULL || isa == NULL) {
+		return BK_ALLOCATION_ERROR;
+	}
+
+	if (isa -> instanceSize != guardSize) {
+		return BK_ALLOCATION_ERROR;
+	}
+
+	memset (obj, 0, isa -> instanceSize);
+
+	obj -> flags = BKObjectFlagInitialized;
+	obj -> isa   = isa;
+
+	return 0;
+}
+
+BKInt BKObjectAlloc (void ** outObject, BKClass const * isa, BKSize extraSize)
+{
+	BKObject * obj;
+
+	if (isa == NULL || isa -> instanceSize == 0) {
+		return BK_ALLOCATION_ERROR;
+	}
+
+	obj = malloc (isa -> instanceSize + extraSize);
+
+	if (obj == NULL) {
+		return BK_ALLOCATION_ERROR;
+	}
+
+	memset (obj, 0, isa -> instanceSize);
+
+	obj -> flags = BKObjectFlagInitialized;
+	obj -> isa   = isa;
+
+	*outObject = obj;
+
+	return 0;
+}
+
+BKInt BKSetAttr (void * object, BKEnum attr, BKInt value)
+{
+	BKObject * obj = object;
+	BKClass const * isa;
+
+	if (obj == 0) {
+		return BK_INVALID_STATE;
+	}
+
+	isa = obj -> isa;
+
+	if (isa -> setAttr == NULL) {
+		return BK_INVALID_STATE;
+	}
+
+	return isa -> setAttr (obj, attr, value);
+}
+
+BKInt BKGetAttr (void const * object, BKEnum attr, BKInt * outValue)
+{
+	BKObject const * obj = object;
+	BKClass const * isa;
+
+	if (obj == 0) {
+		return BK_INVALID_STATE;
+	}
+
+	isa = obj -> isa;
+
+	if (isa -> getAttr == NULL) {
+		return BK_INVALID_STATE;
+	}
+
+	return isa -> getAttr (obj, attr, outValue);
+
+}
+
+BKInt BKSetPtr (void * object, BKEnum attr, void * ptr, BKSize size)
+{
+	BKObject * obj = object;
+	BKClass const * isa;
+
+	if (obj == 0) {
+		return BK_INVALID_STATE;
+	}
+
+	isa = obj -> isa;
+
+	if (isa -> setPtr == NULL) {
+		return BK_INVALID_STATE;
+	}
+
+	return isa -> setPtr (obj, attr, ptr, size);
+}
+
+BKInt BKGetPtr (void const * object, BKEnum attr, void * outPtr, BKSize size)
+{
+	BKObject const * obj = object;
+	BKClass const * isa;
+
+	if (obj == 0) {
+		return BK_INVALID_STATE;
+	}
+
+	isa = obj -> isa;
+
+	if (isa -> getPtr == NULL) {
+		return BK_INVALID_STATE;
+	}
+
+	return isa -> getPtr (obj, attr, outPtr, size);
+}
+
+void BKDispose (void * object)
+{
+	BKObject * obj = object;
+	BKClass const * isa = obj -> isa;
+	BKUInt flags = obj -> flags;
+
+	if (obj == NULL || (flags & BKObjectFlagInitialized) == 0) {
+		return;
+	}
+
+	if (isa && isa -> dispose) {
+		isa -> dispose (obj);
+	}
+
+	memset (obj, 0, isa -> instanceSize);
+
+	if (flags & BKObjectFlagAllocated) {
+		free (obj);
+	}
+}
---
+++ src/BKObject.h
@@ -0,0 +1,159 @@
+/**
+ * Copyright (c) 2012-2014 Simon Schoenenberger
+ * http://blipkit.monoxid.net/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef _BK_OBJECT_H_
+#define _BK_OBJECT_H_
+
+#include "BKBase.h"
+
+/**
+ * Object types
+ */
+typedef struct BKClass  BKClass;
+typedef struct BKObject BKObject;
+
+/**
+ * Instance functions
+ */
+typedef BKInt (* BKSetAttrFunc) (void * object, BKEnum attr, BKInt value);
+typedef BKInt (* BKGetAttrFunc) (void const * object, BKEnum attr, BKInt * outValue);
+typedef BKInt (* BKSetPtrFunc)  (void * object, BKEnum attr, void * ptr, BKSize size);
+typedef BKInt (* BKGetPtrFunc)  (void const * object, BKEnum attr, void * outPtr, BKSize size);
+typedef BKInt (* BKDisposeFunc) (void * object);
+
+/**
+ * Object flags
+ */
+enum BKObjectFlag
+{
+	BKObjectFlagInitialized = 1 << 24,
+	BKObjectFlagAllocated   = 1 << 25,
+};
+
+/**
+ * Class
+ */
+struct BKClass
+{
+	BKUInt        flags;
+	BKSize        instanceSize;
+	BKDisposeFunc dispose;
+	BKSetAttrFunc setAttr;
+	BKGetAttrFunc getAttr;
+	BKSetPtrFunc  setPtr;
+	BKGetPtrFunc  getPtr;
+};
+
+/**
+ * Abstract object
+ */
+struct BKObject
+{
+	BKUInt flags;
+	BKClass const * isa;
+};
+
+/**
+ * Initialize object
+ *
+ * Empty given struct and set class `isa`
+ * `guardSize` is checked against the class' instance size to prevent overflow
+ * Returns 0 on success
+ *
+ * Return errors:
+ * BK_ALLOCATION_ERROR if `guardSize` differs from instance size given by class
+ */
+extern BKInt BKObjectInit (void * object, BKClass const * isa, BKSize guardSize);
+
+/**
+ * Allocate object
+ *
+ * Allocate and a new object with class `isa`
+ * Optionally `extraSize` bytes are reserved after object
+ * Returns 0 on success
+ *
+ * Return errors:
+ * BK_ALLOCATION_ERROR if allocation failed
+ */
+extern BKInt BKObjectAlloc (void ** outObject, BKClass const * isa, BKSize extraSize);
+
+/**
+ * Set attribute
+ *
+ * Set a single integer attribute
+ * Returns 0 on success
+ *
+ * Return errors:
+ * BK_INVALID_ATTRIBUTE if attribute is unknown by object
+ * BK_INVALID_VALUE if attribute value is invalid
+ * BK_INVALID_STATE if object does not support setting attributes or is NULL
+ */
+extern BKInt BKSetAttr (void * object, BKEnum attr, BKInt value);
+
+/**
+ * Get attribute
+ *
+ * Get a single integer attribute
+ * Returns 0 on success
+ *
+ * Return errors:
+ * BK_INVALID_ATTRIBUTE if attribute is unknown by object
+ * BK_INVALID_STATE if object does not support getting attributesor or is NULL
+ */
+extern BKInt BKGetAttr (void const * object, BKEnum attr, BKInt * outValue);
+
+/**
+ * Set pointer
+ *
+ * Set a pointer attribute with size `size`
+ * Returns 0 on success
+ *
+ * Return errors:
+ * BK_INVALID_ATTRIBUTE if attribute is unknown by object
+ * BK_INVALID_VALUE if attribute value is invalid
+ * BK_INVALID_STATE if object does not support setting pointers or is NULL
+ */
+extern BKInt BKSetPtr (void * object, BKEnum attr, void * ptr, BKSize size);
+
+/**
+ * Get pointer
+ *
+ * Get a pointer attribute with size `size`
+ * Returns 0 on success
+ *
+ * Return errors:
+ * BK_INVALID_ATTRIBUTE if attribute is unknown by object
+ * BK_INVALID_STATE if object does not support getting pointers or is NULL
+ */
+extern BKInt BKGetPtr (void const * object, BKEnum attr, void * outPtr, BKSize size);
+
+/**
+ * Dispose object
+ *
+ * Calls the class' dispose function
+ * If the object was initialized with `BKObjectInit`, the object is emptied
+ * If the object was allocated with `BKObjectAlloc`, the object freed
+ */
+extern void BKDispose (void * object);
+
+#endif /* ! _BK_OBJECT_H_ */
