2700181d92fbbbdb3840e698507b6eb839160894
detomon <simon@monoxid.ch>
2015-04-03 17:27:13 +0200

    Update copyright and URL

--- src/BKWaveFileWriter.h
+++ src/BKWaveFileWriter.h
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2014 Simon Schoenenberger
- * http://blipkit.monoxid.net/
+ * Copyright (c) 2012-2015 Simon Schoenenberger
+ * http://blipkit.audio
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the "Software"),
